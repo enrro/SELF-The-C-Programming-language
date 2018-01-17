d1af277e09b1364523e41e21079e7a5ed49ce9bf
Enrro <edgarbg585@gmail.com>
2018-01-17 13:31:38 -0600

    second exercise

---
+++ chapter 1/Exercise 1-02/escapeChar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    printf("new line \n");
+    printf("tab \t \n");
+    printf("back space \b \n");
+    printf("hello double quote \" \n");
+    printf("backslash \\ \n");
+    printf("e \e \n");
+    printf("v \v ss \n");
+}
