035d66516863acae43fef2d06c21406eb47960c4
Enrro <edgarbg585@gmail.com>
2018-01-17 14:02:50 -0600

    finished excercise 1-03

---
+++ chapter 1/Exercise 1-03/heading.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Header of table \n");
+    printf("%3s %6s \n", "fahr", "celsius");
+    
+    while(fahr<=upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
