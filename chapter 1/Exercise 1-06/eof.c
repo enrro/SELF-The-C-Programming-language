592701d0931beafc5df514557012192d8d5c9b7c
Enrro <edgarbg585@gmail.com>
2018-01-17 19:28:47 -0600

    change name of excercise 1-07

--- chapter 1/Exercise 1-06/eof.c
+++ chapter 1/Exercise 1-06/eof.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*
+verify that the expresion getchar()!=EOF is 0 or 1.
+*/
 main()
 {
    printf("Please enter a character \n");
---
+++ chapter 1/Exercise 1-07/valueOfEOF.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+1.7 Write a program to print the value of eof
+*/
+
+main()
+{
+    printf("The value of EOF is the following %d \n", EOF);
+}
