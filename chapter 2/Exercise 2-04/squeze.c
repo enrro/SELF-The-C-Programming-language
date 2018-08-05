a89ddfa08e877bf9ca6d4fb9d0c3c0321f217ab8
Enrro <edgarbg585@gmail.com>
2018-08-04 23:26:49 -0500

    finished 2.04

--- chapter 2/Exercise 2-04/squeze.c
+++ chapter 2/Exercise 2-04/squeze.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
+#define MAXLINE 1000
 
-void squeeze(char a[], char b[]){
-    int i, j, k;
+void squeeze(char a[], char b[]);
+void getlinee(char line[],int maxline);
 
-    for(i = k = 0; a[i] != '\0'; i++){
-        for(j = 0; b[j] != '\0'; j++){
-            if( b[j] != a[i]){
-                a[k++] = a[i];
-            }
-        }
-    }
-    a[k]='\0';
-    
+int main(){
+    char a[MAXLINE];
+    char b[MAXLINE]; 
+
+    getlinee(a,MAXLINE);
+    getlinee(b,MAXLINE);
+    squeeze(a,b);
+    printf("%s \n", a);
+    return 0;
 }
 
+void squeeze(char a[], char b[]){
+    int i, j, k,r,bo;
+    bo = r = 0;
+    for(i = k = 0; a[i] != '\0'; i++){
+        for(j = 0; b[j] != '\0'; j++){
+            if(a[i] == b[j]){
+                printf("%c , %c \n",a[i], b[j]);
+                r = 1;
+            }
+        }
+        if(r == 0){
+            a[++bo] == a[i];
+        }
+        r = 0; 
+    }
+    a[bo]='\0';
+    printf("%s \n", a);
+}
+
+void getlinee(char line[],int maxline){
+    int c, i, l;
+
+    for (i = 0, l = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+        if (i < maxline - 1)
+            line[l++] = c;
+    line[l] = '\0';
+}
